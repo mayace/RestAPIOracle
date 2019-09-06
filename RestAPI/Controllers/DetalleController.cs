d7c35ab299b1576340c65ca8671d193e04ee8a01
ctejaxun <ctejaxun@infonet.com.gt>
2019-09-06 09:08:43 -0600

    agrega creacion metodo post y delete para cliente producto detalle solo post

--- RestAPI/Controllers/DetalleController.cs
+++ RestAPI/Controllers/DetalleController.cs
@@ -28,5 +28,51 @@ namespace RestAPIOracle.Controllers
             }
         }
 
+
+        [HttpPost]
+        public object Post([FromBody]Models.Detalle data)
+        {
+            try
+            {
+                using (var db = new Models.MyFacturaContext())
+                {
+                    //data.Fecha = DateTime.UtcNow;
+                    //data.IDFactura = null;
+                    db.Detalles.Add(data);
+                    var total = db.SaveChanges();
+                    return new { data, total };
+                }
+            }
+            catch (Exception e)
+            {
+                return new { error = e.ToString() };
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public object Delete(int id)
+        //{
+        //    try
+        //    {
+        //        using (var db = new Models.MyFacturaContext())
+        //        {
+        //            var data = db.Detalles.Where(item => item.i == id);
+        //            var total = 0;
+        //            if (data.Count() > 0)
+        //            {
+        //                db.Clientes.RemoveRange(data);
+        //                total = db.SaveChanges();
+        //            }
+
+        //            return new { data = data.ToList(), total };
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new { error = e.ToString() };
+        //    }
+        //}
+
     }
 }
--- RestAPI/Controllers/ProductoController.cs
+++ RestAPI/Controllers/ProductoController.cs
@@ -28,5 +28,50 @@ namespace RestAPIOracle.Controllers
             }
         }
 
+        [HttpPost]
+        public object Post([FromBody]Models.Producto data)
+        {
+            try
+            {
+                using (var db = new Models.MyFacturaContext())
+                {
+                    //data.Fecha = DateTime.UtcNow;
+                    //data.IDFactura = null;
+                    db.Productos.Add(data);
+                    var total = db.SaveChanges();
+                    return new { data, total };
+                }
+            }
+            catch (Exception e)
+            {
+                return new { error = e.ToString() };
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public object Delete(int id)
+        {
+            try
+            {
+                using (var db = new Models.MyFacturaContext())
+                {
+                    var data = db.Productos.Where(item => item.IDProducto == id);
+                    var total = 0;
+                    if (data.Count() > 0)
+                    {
+                        db.Productos.RemoveRange(data);
+                        total = db.SaveChanges();
+                    }
+
+                    return new { data = data.ToList(), total };
+                }
+            }
+            catch (Exception e)
+            {
+                return new { error = e.ToString() };
+            }
+        }
+
     }
 }
