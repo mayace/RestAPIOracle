4596939da61a03ce1e7cf221d3779139663ab774
ctejaxun <ctejaxun@infonet.com.gt>
2019-09-05 20:28:11 -0600

    actuliza connection string

--- RestAPI/Models/Factura.cs
+++ RestAPI/Models/Factura.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -22,17 +24,38 @@ namespace RestAPIOracle.Models
         public DateTime FechaRegistro { get; set; }
     }
 
+    public class Producto
+    {
+        [Key]
+        public int IDProducto { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string Nombre { get; set; }
+    }
+
     public class MyFacturaContext : DbContext
     {
+
+        public MyFacturaContext() : base()
+        {
+            //    var creator = Database.GetService<IDatabaseCreator>();
+            //    Database.Migrate();
+        }
+
+        public MyFacturaContext(DbContextOptions<MyFacturaContext> options)
+       : base(options)
+        {
+        }
+
         public DbSet<Factura> Facturas { get; set; }
-        
+        public DbSet<Producto> Productos { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //string connStr = "Data Source=45.32.22.223:49161/xe;User Id=practicas1;Password=practicas114;";
             //https://stackoverflow.com/a/56343155
             optionsBuilder
-                .UseOracle(@"Data Source=45.32.22.223:49161/xe;User Id=practicas1;Password=practicas114;", options => options.UseOracleSQLCompatibility("11"));
+                .UseOracle(@"Data Source=159.65.170.43:49161/xe;User Id=system;Password=oracle;", options => options.UseOracleSQLCompatibility("11"));
                 
         }
     }
